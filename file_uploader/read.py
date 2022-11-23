287402a000568d74fb323f475b89f3b418577424
Samuel Costa <samu.rcosta@gmail.com>
2022-11-22 23:31:05 -0300

    Prevent to download recently created files

--- file_uploader/read.py
+++ file_uploader/read.py
@@ -1,10 +1,23 @@
+import os
+from datetime import datetime
 from glob import glob
 from os import path
 from file_uploader.config import LOCAL_DIR
 
 
+WAIT_CREATED_SECONDS_TO_DOWNLOAD = 15
+
+
+def if_valid_for_download(filename):
+    t = os.path.getmtime(filename)
+    now = datetime.now()
+    modified = datetime.fromtimestamp(t)
+
+    return (now - modified).seconds > WAIT_CREATED_SECONDS_TO_DOWNLOAD
+
+
 def get_files():
     print(LOCAL_DIR)
     files = glob(f"{LOCAL_DIR}/**/*", recursive=True)
-    files = [f for f in files if path.isfile(f)]
+    files = [f for f in files if path.isfile(f) and if_valid_for_download(f)]
     return files
